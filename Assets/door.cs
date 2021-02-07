fc8ebe1156a6c8be8a065c2ef7473ab8582426a1
sky2145 <74121357+sky2145@users.noreply.github.com>
2021-02-07 15:02:10 +0900

    変更

---
+++ Assets/door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    // クリックした時に、鍵を持っていれば Openにする
+    // 持っていなければログを出す
+    public GameObject openObj;
+
+    public void OnThis()
+    {
+        bool hasKey = ItemBox.instance.CanUseItem(ItemManager.Item.Key);
+        if (hasKey == true)
+        {
+            Open();
+            ItemBox.instance.UseItem(ItemManager.Item.Key);
+        }
+        else
+        {
+            Debug.Log("鍵がかかっている");
+        }
+    }
+
+    void Open()
+    {
+        // 開いている画像を表示
+        openObj.SetActive(true);
+    }
+}
